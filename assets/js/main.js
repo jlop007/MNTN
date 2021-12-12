576c0e88c7ee58b77b5272da8a36dd59ada06f79
Julio <j-l20@live.com>
2021-12-12 07:45:41 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Variable declarations
+const openMenuIcon = document.getElementById('menu-toggle-open');
+const closeMenuIcon = document.getElementById('menu-toggle-close');
+const mobileMenu = document.getElementById('mobile-menu');
+const headerOverlay = document.getElementById('header');
+const grassImage = document.getElementById('grassland');
+const skyImage = document.getElementById('sky');
+const mountainsImage = document.getElementById('mountains');
+const heroData = document.getElementById('hero-data');
+
+// Open menu & add overlay
+
+// Close menu & remove overlay
+
+// Add parallax effect
